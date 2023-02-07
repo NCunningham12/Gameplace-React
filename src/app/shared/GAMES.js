e979bce3f318827dc52f5f96955356fa7d4f2fdc
Nathan Cunningham <counterweight22@gmail.com>
2023-02-06 20:50:45 -0800

    Create Header/Footer. Begin building GAMES array locally

---
+++ src/app/shared/GAMES.js
@@ -0,0 +1,18 @@
+// import game thumbnails here
+import sekiroThumbnail from "../assets/img/thumbnails/sekiroThumbnail.jpg";
+
+// create object array with all games here
+export const GAMES = [
+  {
+    id: 0,
+    name: "Sekiro: Shadows Die Twice",
+    image: sekiroThumbnail,
+    developer: "FromSoft",
+    rating: 10 / 10,
+    featured: true,
+    goty: true,
+    platform: ["PC", "Xbox", "PS"],
+    description:
+      "In Sekiro: Shadows Die Twice you are the “one-armed wolf”, a disgraced and disfigured warrior rescued from the brink of death. Bound to protect a young lord who is the descendant of an ancient bloodline, you become the target of many vicious enemies, including the dangerous Ashina clan.",
+  },
+];
